26c570fc22024a73e1c1b69c7faf5cb266c63248
Alex Oliveira <alex@dnasistemas.com>
2018-04-15 10:40:20 -0300

    Aula Ruby

--- ruby/metodos.rb
+++ ruby/metodos.rb
@@ -2,6 +2,13 @@
 
 def login(user, password)
   puts "usuario #{user} logando no sistema"
+  if user == "root"
+    puts "usuario admin detectado"
+  elseif user == "admin"
+    puts "agora é o admin que ta logando."
+  else
+    puts "outro mane logando"
+  end
 end
 
 def logout(session_id)
