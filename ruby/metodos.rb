e9b37976ea42981aec7e3e09dbf597b5ca9fc3ca
Alex Oliveira <alex@dnasistemas.com>
2018-04-15 10:24:38 -0300

    Aula Ruby

---
+++ ruby/metodos.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+def login(user, password)
+  puts "usuario #{user} logando no sistema"
+end
+
+def logout(session_id)
+  puts "logout da sessao #{session_id}"
+end
+
---
+++ ruby/sistema.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+
+require './metodos'
+
+login(`whoami`, "changeit")
+logout('hauid2389yfdas')
