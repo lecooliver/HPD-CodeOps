194699ff913187b618d4407d5809f4212ffc70b6
Alex Oliveira <alex@dnasistemas.com>
2018-04-28 10:05:34 -0300

    Python

---
+++ python/parser.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("giropops", help="Comando para possuir mentes", type=int)
+
+args = parser.parse_args()
+
+print(args.giropops)
